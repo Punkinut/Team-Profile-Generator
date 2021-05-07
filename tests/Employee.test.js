62b1be6189de562774089a5db2d2290e5bc94cd0
Punkinut <heypunkinut@gmail.com>
2021-05-07 16:28:09 +1000

    Added three successful tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,10 +1,28 @@
 const Employee = require('../lib/employee');
 
 describe("Class Test Sequence", () => {
-    it('Should set a name via constructor arguements', () => {
+    it('Can set name via constructor arguements', () => {
         let name = 'Josh';
 
         let employ = new Employee(name);
         expect(employ.name).toEqual('Josh');
     })
+    it('Can set id via constructor arguements', () => {
+        let id = '3';
+
+        let employ = new Employee(' ', id);
+        expect(employ.id).toEqual('3');
+    })
+    it('Can set email via constructor arguements', () => {
+        let email = 'example@gmail.com';
+
+        let employ = new Employee(' ', ' ', email);
+        expect(employ.email).toEqual('example@gmail.com');
+    })
+    // it('Can get name via getName()', () => {
+    //     let name = 'Smith';
+
+    //     let employ = new Employee(name);
+    //     expect(employ.name).toEqual('example@gmail.com');
+    // })
 })
