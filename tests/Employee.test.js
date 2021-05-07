04c23e2676a1961c7b0c5c8a847c7c62da756fdd
Punkinut <heypunkinut@gmail.com>
2021-05-07 16:06:08 +1000

    Wrote first test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,10 +1,10 @@
 const Employee = require('../lib/employee');
 
 describe("Class Test Sequence", () => {
-    it('Should set a name via constructor arguements'), () => {
+    it('Should set a name via constructor arguements', () => {
         let name = 'Josh';
 
         let employ = new Employee(name);
         expect(employ.name).toEqual('Josh');
-    }
+    })
 })
